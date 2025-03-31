fb5312b7def6ceff57985ae230a44e61e1667c08
Elena Ferreras <82446536+elenaferreras@users.noreply.github.com>
2025-03-31 09:02:04 +0000

    updates to build.js

--- build.js
+++ build.js
@@ -1,4 +1,32 @@
+import StyleDictionary from 'style-dictionary';
+import { register } from '@tokens-studio/sd-transforms';
+import ThemesLoader from 'sd-themes-loader';
+
+register(StyleDictionary, {
+    withSDBuiltins: false,
+});
+
+const loader = ThemesLoader(StyleDictionary);
+
 async function run() {
+    const themes = await loader.load('/tokens');
+
+    const globalTheme = themes.getThemeByName('global');
+
+    const config = {
+        platforms: {
+            web: {
+                files: [
+                    {
+                        destination: 'app/build/global/variables.css',
+                        format: 'css/variables',
+                    }
+                ],
+            }
+        }
+    };
+
+    globalTheme.addConfig(config).build();
 
 }
 
